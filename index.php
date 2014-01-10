6018682b74e28bf84fed5ddf728741c439c0d368
minimaldev <yaraujo@webdisenoya.com>
2014-01-09 22:49:40 -0430

    building files

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+
+//protejer contra xss
+function xssafe($data,$encoding='UTF-8')
+{
+	return htmlspecialchars($data,ENT_QUOTES,$encoding);
+}
+
+function Request($item,$default='')
+{
+	if (isset($_REQUEST[$item]))
+		return xssafe($_REQUEST[$item]);
+	else
+		return xssafe($default);
+}
+
+
+function prueba($name='a') 
+{
+	
+	header('Content-Type: text/html; charset=utf-8');
+	
+	$input = Request('hh','no name');	
+	
+	printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+	
+}
+
+function homepage() 
+{
+    print "HOMEPAGE";
+}
+
+function error_404() 
+{
+    print "error 404";
+}
+
+$uri = Request('_url','/');
+
+$patterns=array(
+	
+	'/test'			=>	'prueba',
+	'/test/{name}'	=>	'prueba',
+	'/'				=>	'homepage',
+
+);
+
+$server_pages=array(
+	
+	'404'	=>	'error_404',
+	'500'	=>	'homepage',
+
+);
+
+//verificamos si existe
+
+if (array_key_exists($uri, $patterns)) 
+{
+	//llamamos a la function
+	call_user_func ( $patterns[$uri]);
+}
+else
+{	
+	//si no se consigue llamamos error 404
+	call_user_func ( $server_pages['404']);
+}
+
